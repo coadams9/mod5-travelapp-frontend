230f095ab25043a273d93db6ff6ef699023cca0f
Cory <coadams9@gmail.com>
2019-08-28 00:45:30 -0400

    still trying jess

--- src/actions/index.js
+++ src/actions/index.js
@@ -20,12 +20,11 @@ import plane18 from '../images/plane18.jpg'
 
 
 
-// const KEY = process.env.REACT_APP_KEY
-debugger
+const KEY = process.env.REACT_APP_KEY
+
 
 export function searchTerm1(value) {
   return dispatch => {
-    debugger
     fetch(`https://apidojo-kayak-v1.p.rapidapi.com/locations/search?where=/${value}`, {
       headers: {
         "Content-Type": "application/json",
@@ -34,7 +33,6 @@ export function searchTerm1(value) {
     })
       .then(res => res.json())
       .then(data => {
-        debugger
         dispatch({ type: 'DEPARTURES', data })
       })
     dispatch({ type: 'SRCH_LOCT1', value })
