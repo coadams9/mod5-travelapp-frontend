5893106bb17106485b0f52b0f4fa42572bfe91c8
Cory <coadams9@gmail.com>
2019-10-25 15:45:11 -0400

    reformated login tests

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,13 +2,7 @@ import login from '../support/pages/login-page-test'
 
 
 describe("Logging in", () => {
-  it("successfully", () => {
-    cy.visit('http://localhost:3001')
-    login.link().click()
-    login.username().type('Cory')
-    login.password().type('1234')
-    login.button().click()
-
-    cy.location('pathname').should('eq', '/home')
+  it("successfully!", () => {
+    cy.login('Cory', '1234')
   })
 })
